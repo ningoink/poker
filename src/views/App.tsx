9f360dc75fb6f5ea68c25a0a4b49992624b33cf0
hylerrix <hylerrix@foxmail.com>
2019-12-02 15:55:00 +0800

    style: Remove unnecessary semi-colons
    Signed-off-by: hylerrix <hylerrix@foxmail.com>

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from '../logo.svg'
+import './App.css'
 
 const App: React.FC = () => {
   return (
@@ -20,7 +20,7 @@ const App: React.FC = () => {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
